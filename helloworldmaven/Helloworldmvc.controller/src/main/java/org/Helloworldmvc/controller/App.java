7eb21d2619f2160eac2c93f6b65002dc755fb73e
sambao407 <sambao407@gmail.com>
2017-06-06 20:21:00 +0200

    Phase 2

---
+++ helloworldmaven/Helloworldmvc.controller/src/main/java/org/Helloworldmvc/controller/App.java
@@ -0,0 +1,13 @@
+package org.Helloworldmvc.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
